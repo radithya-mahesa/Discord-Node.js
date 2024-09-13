842ea76698cc10e531c235cab9d2b65e49658cdd
radithya-mahesa <radithya.syabil.com>
2024-09-13 14:10:45 +0700

    feat: add alive.js

---
+++ alive.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const http = require('http')
+const PORT = 5000
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Bot Is Alive ✅')
+})
+
+function alive() {
+  const server = http.createServer(app)
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}\nhttp://localhost:${PORT}`)
+  })
+}
+
+module.exports = alive
